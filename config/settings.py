de845ff266c062c2991a77004a956c6688b0244d
gilbertopir <47357164+gilbertopir@users.noreply.github.com>
2025-10-20 16:49:29 +0100

    Update settings.py

--- config/settings.py
+++ config/settings.py
@@ -69,6 +69,7 @@ TEMPLATES = [
 
 WSGI_APPLICATION = 'config.wsgi.application'
 
+ALLOWED_HOSTS = ['playthatsong.onrender.com']
 
 # Database
 # https://docs.djangoproject.com/en/5.2/ref/settings/#databases
@@ -121,3 +122,4 @@ STATIC_URL = 'static/'
 # https://docs.djangoproject.com/en/5.2/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
